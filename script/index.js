b8e01f1c49f5f36ccab94e4e3c36bd817da9962c
alswjd370304-sys <alswjd370304@gamil.com>
2026-07-12 14:22:04 +0900

    탄생화 앱 과제

---
+++ script/index.js
@@ -0,0 +1,27 @@
+/* index.js */
+/* 1.목표: 팝업의 배경을 누르면 사라지고 다시 버튼을 누르면 뜨도록 함. */
+const popup = document.querySelector ('.popup_wrap');
+const popup_bg = document.querySelector ('.popup_bg');
+console.log(popup);
+console.log(popup_bg);
+
+popup.style.display = 'none';
+popup_bg.style.display = 'none';
+
+function popup_open() {
+    popup.style.display = 'flex';
+    popup_bg.style.display = 'block';
+}
+
+function popup_hide() {
+    popup.style.display = 'none';
+    popup_bg.style.display = 'none';
+}
+
+const birthday_month = document.querySelector('#birthday_write');
+birthday_month.vaule = '12';
+console.log(birthday_month);
+function birthday_month_write() {
+    const birthday_month = document.querySelector('#birthday_write');
+    birthday_month.value = '12';
+}

1b0115ba96e0334d8f2475d06967ad8c352bbd31
alswjd370304-sys <alswjd370304@gamil.com>
2026-07-13 21:32:06 +0900

    탄생화 앱 과제 완성

--- script/index.js
+++ script/index.js
@@ -17,11 +17,10 @@ function popup_hide() {
     popup.style.display = 'none';
     popup_bg.style.display = 'none';
 }
-
+/* 2. 목표: #birthday_write를 누르면 값 12가 입력 되도록 한다. */
 const birthday_month = document.querySelector('#birthday_write');
 birthday_month.vaule = '12';
 console.log(birthday_month);
 function birthday_month_write() {
-    const birthday_month = document.querySelector('#birthday_write');
     birthday_month.value = '12';
 }

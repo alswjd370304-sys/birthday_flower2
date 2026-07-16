afdc5417771dbb1a2a2d5bce0d18542ada1fd7af
alswjd370304-sys <alswjd370304@gmail.com>
2026-07-16 10:51:19 +0900

    탄생화 앱 수정

--- script/index.js
+++ script/index.js
@@ -17,9 +17,13 @@ function popup_hide() {
     popup.style.display = 'none';
     popup_bg.style.display = 'none';
 }
+// 자바스크립트에서 diplay=none으로 숨겼다가 특정 이벤트애 따라 다시 보이고 싶을 때
+// 무조건 block을 쓰는게 아닌 그 선택자가 원래 가지고 있는  display속성으로 되돌리기
+// display:flex가 선택자에 있었다면 자바스크립트도 display:flex
+// 없었다면 고유 속성을 기입 (inline, block)
 /* 2. 목표: #birthday_write를 누르면 값 12가 입력 되도록 한다. */
 const birthday_month = document.querySelector('#birthday_write');
-birthday_month.vaule = '12';
+// birthday_month.value = '12';
 console.log(birthday_month);
 function birthday_month_write() {
     birthday_month.value = '12';
